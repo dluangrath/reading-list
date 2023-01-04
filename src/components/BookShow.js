1f51907a353d7ca2a9ad1edeeaf8086c8db82d54
dluangrath <dluangrath9@gmail.com>
2023-01-04 00:03:19 -0800

    added random images to books

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -14,7 +14,7 @@ function BookShow({ book, onDelete, onEdit }) {
 
     const handleSubmit = (id, newTitle) => {
         setShowEdit(false);
-        onEdit(id, newTitle)
+        onEdit(id, newTitle);
     };
 
     let content = <h3>{book.title}</h3>;
@@ -23,6 +23,7 @@ function BookShow({ book, onDelete, onEdit }) {
     }
 
     return <div className="book-show">
+        <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
         <div>{content}</div>
         <div className="actions">
             <button className="edit" onClick={handleEditClick}>
